a648a453e087c84259c4a1f9d9448d6a74a94943
lathikac333 <111533549+lathikac333@users.noreply.github.com>
2023-08-26 22:59:14 +1000

    Adjusted text space for menu bar

--- menu.php
+++ menu.php
@@ -1,11 +1,11 @@
 <div class="menu">
 <ul>
-<li <?php if($current == 'home') {echo 'class="current"';}?>> <a href=" home.php">HOME</a></li>
-<li <?php if($current == 'about') {echo 'class="current"';}?>> <a href=" about.php">ABOUT US</a></li>
-<li <?php if($current == 'coursedetails') {echo 'class="current"';}?>> <a href=" coursedetails.php">COURSE DETAILS</a></li>
-<li <?php if($current == 'register') {echo 'class="current"';}?>> <a href="register.php">STUDENT REGISTER</a></li> 
-<li <?php if($current == 'login') {echo 'class="current"';}?>> <a href=" login.php">COURSE REGISTRATION</a></li> 
-<li <?php if($current == 'newsnotices') {echo 'class="current"';}?>> <a href=" newsnotices.php">NEWS & NOTICES</a> </li>
-<li <?php if($current == 'contact') {echo 'class="current"';}?>> <a href=" contact.php">CONTACT US</a> </li>
+<li <?php if($current == 'home') {echo 'class="current"';}?>> <a href=" home.php">&nbsp &nbsp &nbsp &nbsp HOME &nbsp &nbsp &nbsp &nbsp</a></li>
+<li <?php if($current == 'about') {echo 'class="current"';}?>> <a href=" about.php">&nbsp &nbsp &nbsp &nbsp  ABOUT US &nbsp &nbsp &nbsp &nbsp</a></li>
+<li <?php if($current == 'coursedetails') {echo 'class="current"';}?>> <a href=" coursedetails.php">&nbsp &nbsp &nbsp &nbsp COURSE DETAILS &nbsp &nbsp &nbsp &nbsp</a></li>
+<li <?php if($current == 'register') {echo 'class="current"';}?>> <a href="register.php">&nbsp &nbsp &nbsp &nbsp STUDENT REGISTER &nbsp &nbsp &nbsp &nbsp</a></li> 
+<li <?php if($current == 'login') {echo 'class="current"';}?>> <a href=" login.php">&nbsp &nbsp &nbsp COURSE REGISTRATION &nbsp &nbsp &nbsp</a></li> 
+<li <?php if($current == 'newsnotices') {echo 'class="current"';}?>> <a href=" newsnotices.php">&nbsp &nbsp &nbsp &nbsp NEWS & NOTICES &nbsp &nbsp &nbsp &nbsp</a> </li>
+<li <?php if($current == 'contact') {echo 'class="current"';}?>> <a href=" contact.php">&nbsp &nbsp &nbsp &nbsp CONTACT US &nbsp &nbsp &nbsp &nbsp</a> </li>
 </ul>
 </div>
